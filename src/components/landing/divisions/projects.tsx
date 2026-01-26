b6cd047adc593d26feb05debc69b599b190c2c3b
Fray117 <fray117@exploiter.id>
2026-01-27 02:55:20 +0700

    add department projects

---
+++ src/components/landing/divisions/projects.tsx
@@ -0,0 +1,15 @@
+import Card from "@/components/landing/divisions/card"
+import { divisions, projects } from "@/projects"
+
+export default function Projects() {
+	const units = [...divisions, ...projects]
+	return (
+		<div className="mx-16 grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-5 gap-8 px-4 xl:px-0">
+			{units.map((unit, index) => (
+				<Card key={index} title={unit.title} logo={unit.logo} link={unit.link} href={unit.href} label={unit.label} ambient={unit.ambient}>
+					<span dangerouslySetInnerHTML={{ __html: unit.description }} />
+				</Card>
+			))}
+		</div>
+	)
+}
