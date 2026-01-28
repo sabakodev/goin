abf6eeac7b7fc777997154ecd168966723830759
Fray117 <fray117@exploiter.id>
2026-01-28 14:21:00 +0700

    update kidung header layout

--- src/components/kidung/header.tsx
+++ src/components/kidung/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function HeaderKidungan() {
 	return (
-		<Link href="/kidung" className="mx-6 lg:mx-24 mt-4 font-serif text-3xl flex items-center space-x-4">
+		<Link href="/kidung" className="mx-6 lg:mx-24 mt-4 font-serif text-3xl flex flex-col sm:flex-row items-center space-x-4">
 			<Image src="/logo/goi.png" width={80} height={80} alt="GOI" />
 			<h1>Standarisasi Kidung GOI</h1>
 		</Link>
