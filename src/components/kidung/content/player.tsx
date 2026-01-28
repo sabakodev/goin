29b4d6ff0549f500c28240a9a2b81b295fe51cdf
Fray117 <fray117@exploiter.id>
2026-01-28 14:19:16 +0700

    add link to button as redundant

--- src/components/kidung/content/player.tsx
+++ src/components/kidung/content/player.tsx
@@ -1,4 +1,5 @@
 import { PlayIcon } from "@heroicons/react/24/solid"
+import Link from "next/link"
 
 export default function KidungPlayer({ embed }: { embed: string }) {
 	return (
@@ -6,10 +7,12 @@ export default function KidungPlayer({ embed }: { embed: string }) {
 			{
 				embed === '' ? null : <iframe className="mt-4 max-w-sm lg:max-w-7xl" width="560" height="315" src={embed} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen />
 			}
-			<div className="my-8 rounded-full bg-neutral-900 hover:bg-neutral-700 py-2 px-4 space-x-2 flex cursor-pointer shadow-sm hover:shadow-lg transition duration-300">
-				<PlayIcon className="size-6 text-secondary" />
-				<span className="text-primary font-semibold">Putar Contoh Nada</span>
-			</div>
+			<Link href={embed}>
+				<div className="my-8 rounded-full bg-neutral-900 hover:bg-neutral-700 py-2 px-4 space-x-2 flex cursor-pointer shadow-sm hover:shadow-lg transition duration-300">
+					<PlayIcon className="size-6 text-secondary" />
+					<span className="text-primary font-semibold">Putar Contoh Nada</span>
+				</div>
+			</Link>
 		</>
 	)
 }
