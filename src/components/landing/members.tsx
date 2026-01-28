3d9b79211e28ab8d233b02e7189087ad5e682f08
Fray117 <fray117@exploiter.id>
2026-01-28 13:52:29 +0700

    fix members section layout

--- src/components/landing/members.tsx
+++ src/components/landing/members.tsx
@@ -1,7 +1,7 @@
 export default function Members() {
 	return (
 		<div className="font-serif h-screen flex justify-center items-center bg-[url(/assets/overlay/indonesian-map.svg)] bg-no-repeat bg-cover bg-center">
-			<p className="max-w-xs lg:max-w-3xl text-5xl">
+			<p className="max-w-xs sm:max-w-xl lg:max-w-3xl text-5xl">
 				GOIN berisikan ±100 umat awam maupun katekis antar parokia dan komox seluruh Indonesia, beberapa di Jerman dan Amerika Serikat—dibina oleh para Klerus lintas daerah yang juga ada didalam komunitas.
 			</p>
 		</div>
