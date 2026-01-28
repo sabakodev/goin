b40abb968f9fcbf656acea3b9f3ed395ac785ef1
Fray117 <fray117@exploiter.id>
2026-01-28 13:53:53 +0700

    update landing hero facts

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,9 +6,9 @@ import Pitch from "@/components/landing/pitch"
 
 export default function Home() {
 	const fact = [
-		'GOIN dibentuk oleh umat GOI atas izin Romo Presbiter Yakobus dari Paroki Js. Petrus & Paulus.',
-		'Untuk sementara, grup ini dikelola oleh Mikhael Alexander, jemaat dari Paroki Js. Petrus & Paulus.',
-		'GOIN didanai oleh para inisiator dan anggotanya, difunding independen, lepas dari kas GOI.',
+		'GOIN dibentuk oleh umat GOI atas izin Romo Presbiter Yakobus dari Paroki Js. Petrus & Paulus sebagai Ketua Umum Aras.',
+		'Untuk sementara, grup ini dikelola oleh HA. Mikhael Alexander, jemaat dari Paroki Js. Petrus & Paulus.',
+		'GOIN didanai oleh para inisiator dan anggotanya, funding independen, lepas dari kas GOI.',
 		'GOIN hanya untuk umat yang sudah dibaptis dan katekumen yang menjalani kelas.',
 	]
 	return (
