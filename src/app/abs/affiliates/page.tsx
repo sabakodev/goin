8ef98e95e24d9accb6d7c8eae72de913b6187096
Fray117 <fray117@exploiter.id>
2026-01-28 17:39:57 +0700

    adjust to use utilise infinite scroll ability

--- src/app/abs/affiliates/page.tsx
+++ src/app/abs/affiliates/page.tsx
@@ -1,15 +1,32 @@
 import Hero from "@/components/abs/hero"
 import { BottomPitchSection } from "@/components/abs/pitch"
-import { BusinessAffiliatesLists } from "@/components/abs/projects"
+import { BusinessAffiliatesLists } from "@/components/abs/projectsInfiniteScroll"
+import { AffiliatesQuery } from "@/components/abs/query"
+import { Business } from "@/gql/graphql"
+import { fetchGraphQL } from "@/utils/fetchGraphQL"
+import { print } from "graphql/language/printer"
 
-export default function BusinessAffiliatesPage() {
+export const revalidate = 60
+
+export default async function BusinessAffiliatesPage() {
+	const { businesses } = await fetchGraphQL<{
+		businesses: {
+			nodes: Business[]
+			pageInfo: { hasNextPage: boolean; endCursor: string }
+		}
+	}>(
+		print(AffiliatesQuery), {
+		first: 10,
+	},
+	)
+
 	return (
 		<div className="bg-blue-dark pt-36 sm:pt-0">
 			<section className="bg-blue-darkest">
 				<Hero>
 					List aktif pekerja <i>professional</i>, penyedia jasa, pembuat produk, dan pemilik bisnis Paroki & Region GOI di seluruh Indonesia dan dunia.
 				</Hero>
-				<BusinessAffiliatesLists />
+				<BusinessAffiliatesLists initialData={businesses.nodes} initialCursor={businesses.pageInfo.endCursor} hasMore={businesses.pageInfo.hasNextPage} />
 				<BottomPitchSection />
 			</section>
 		</div>
